618c7fbbfb5a2230c996b8b98270f3442b5fcd2a
Alejandro Galue <agalue@opennms.org>
2019-10-11 09:01:50 -0400

    Introducing tools
    - Move event-watcher to the tools directory.
    - Create a tool to generate requisitions based on current Pods on the
      opennms namespace, to monitor the monitoring solution.

---
+++ tools/generate-requisitions/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"github.com/OpenNMS/onmsctl/model"
+	"github.com/OpenNMS/onmsctl/rest"
+	"github.com/OpenNMS/onmsctl/services"
+)
+
+func main() {
+	flag.StringVar(&rest.Instance.URL, "url", "https://onms.aws.agalue.net/opennms", "OpenNMS URL")
+	flag.StringVar(&rest.Instance.Username, "user", rest.Instance.Username, "OpenNMS Username")
+	flag.StringVar(&rest.Instance.Password, "passwd", rest.Instance.Password, "OpenNMS Password")
+	kubecfg := flag.String("config", os.Getenv("HOME")+"/.kube/config", "Kubernetes Configuration")
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubecfg)
+	if err != nil {
+		panic(err)
+	}
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	pods, err := client.CoreV1().Pods("opennms").List(v1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	requisition := model.Requisition{
+		Name: "Kubernetes",
+	}
+	for _, pod := range pods.Items {
+		intf := &model.RequisitionInterface{
+			IPAddress:   pod.Status.PodIP,
+			SnmpPrimary: "N",
+			Status:      1,
+		}
+		if value, ok := pod.ObjectMeta.Labels["app"]; ok {
+			switch value {
+			case "kafka":
+				intf.AddService(&model.RequisitionMonitoredService{Name: "JMX-Kafka"})
+			case "cassandra":
+				intf.AddService(&model.RequisitionMonitoredService{Name: "JMX-Cassandra"})
+				intf.AddService(&model.RequisitionMonitoredService{Name: "JMX-Cassandra-Newts"})
+			case "postgres":
+				intf.AddService(&model.RequisitionMonitoredService{Name: "PostgreSQL"})
+			case "elasticsearch":
+				intf.AddService(&model.RequisitionMonitoredService{Name: "Elasticsearch"})
+			}
+		}
+		node := &model.RequisitionNode{
+			ForeignID: pod.Name,
+			NodeLabel: pod.Name,
+			Location:  "Kubernetes",
+		}
+		node.AddInterface(intf)
+		node.AddMetaData("hostIP", pod.Status.HostIP)
+		for key, value := range pod.ObjectMeta.Labels {
+			node.AddMetaData(key, value)
+		}
+		requisition.AddNode(node)
+	}
+	svc := services.GetRequisitionsAPI(rest.Instance)
+	err = svc.SetRequisition(requisition)
+	if err != nil {
+		panic(err)
+	}
+	err = svc.ImportRequisition(requisition.Name, "true")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Done!")
+}
