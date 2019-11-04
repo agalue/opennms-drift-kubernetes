9b8060e06586104dbec1f4cecae48e8be9b211a2
Alejandro Galue <agalue@opennms.org>
2019-11-04 09:28:11 -0500

    Adding setup-istio-knative.sh

---
+++ serverless/setup-istio-knative.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+set -e
+
+NO_COLOR=${NO_COLOR:-""}
+if [ -z "$NO_COLOR" ]; then
+  header=$'\e[1;33m'
+  reset=$'\e[0m'
+else
+  header=''
+  reset=''
+fi
+
+if [ "${slack_url}" == "" ]; then
+  echo "ERROR: the slack_url environment variable is required"
+  exit 1
+fi
+
+serving_version="v0.10.0"
+eventing_version="v0.10.0"
+istio_version="1.3.3"
+kafka_server="kafka.opennms.svc.cluster.local:9092"
+
+function header_text {
+  echo "$header$*$reset"
+}
+
+header_text "Starting Knative on minikube..."
+
+header_text "Using Knative Serving Version:  ${serving_version}"
+header_text "Using Knative Eventing Version: ${eventing_version}"
+header_text "Using Istio Version:            ${istio_version}"
+header_test "Using Kafka Server:             ${kafka_server}"
+
+header_text "Labeling default namespace w/ istio-injection=enabled"
+kubectl label namespace default istio-injection=enabled
+
+header_text "Setting up Istio"
+kubectl apply -f "https://raw.githubusercontent.com/knative/serving/${serving_version}/third_party/istio-${istio_version}/istio-crds.yaml"
+kubectl apply -f "https://raw.githubusercontent.com/knative/serving/${serving_version}/third_party/istio-${istio_version}/istio-lean.yaml"
+
+header_text "Waiting for istio to become ready"
+sleep 10; while echo && kubectl get pods -n istio-system | grep -v -E "(Running|Completed|STATUS)"; do sleep 10; done
+
+header_text "Setting up Knative Serving"
+kubectl apply -f "https://github.com/knative/serving/releases/download/${serving_version}/serving.yaml"
+
+header_text "Waiting for Knative Serving to become ready"
+sleep 10; while echo && kubectl get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 10; done
+
+header_text "Setting up Knative Eventing"
+kubectl apply -f "https://github.com/knative/eventing/releases/download/${eventing_version}/release.yaml"
+kubectl apply -f "https://github.com/knative/eventing-contrib/releases/download/${eventing_version}/kafka-source.yaml"
+
+header_text "Waiting for Knative Eventing to become ready"
+sleep 5; while echo && kubectl get pods -n knative-eventing | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
+sleep 5; while echo && kubectl get pods -n knative-sources | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
+
+header_text "Launching Slack Forwarder Service"
+
+cat <<EOF | kubectl apply -f -
+apiVersion: serving.knative.dev/v1alpha1
+kind: Service
+metadata:
+  name: slack-forwarder
+spec:
+  runLatest:
+    configuration:
+      revisionTemplate:
+        spec:
+          container:
+            image: agalue/slack-forwarder
+            env:
+            - name: SLACK_URL
+              value: ${slack_url}
+            - name: ONMS_URL
+              value: https://onmsui.aws.agalue.net/opennms
+EOF
+
+header_text "Launching Kafka Event Source"
+cat <<EOF | kubectl apply -f -
+apiVersion: sources.eventing.knative.dev/v1alpha1
+kind: KafkaSource
+metadata:
+  name: kafka-source
+spec:
+  consumerGroup: knative-group
+  bootstrapServers: ${kafka_server}
+  topics: OpenNMS-alarms-json
+  sink:
+    apiVersion: serving.knative.dev/v1alpha1
+    kind: Service
+    name: slack-forwarder
+EOF
+
