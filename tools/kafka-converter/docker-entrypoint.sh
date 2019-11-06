4dc21e3ef85d643edad3074135e1428f5a359be4
Alejandro Galue <agalue@opennms.org>
2019-11-06 16:25:40 -0500

    Fixing consumer/producer config parser

--- tools/kafka-converter/docker-entrypoint.sh
+++ tools/kafka-converter/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-function join {
+join() {
   local IFS="$1"; shift; echo "$*";
 }
 
-function get_key() {
-  return $(echo "$1" | cut -d_ -f2- | tr '[:upper:]' '[:lower:]' | tr _ .)
+get_key() {
+  echo "$1" | cut -d_ -f2- | tr '[:upper:]' '[:lower:]' | tr _ .
 }
 
 IFS=$'\n'
@@ -15,12 +15,15 @@ for VAR in $(env)
 do
   env_var=$(echo "$VAR" | cut -d= -f1)
   if [[ $env_var =~ ^CONSUMER_ ]]; then
+    echo "[configuring consumer] processing $env_var"
     key=$(get_key $env_var)
+    echo "[configuring consumer] key: $key"
     val=${!env_var}
-    echo "[configuring consumer] '$key'='$val'"
+    echo "[configuring consumer] value: $val"
     CONSUMER+=("$key=$val")
   fi
   if [[ $env_var =~ ^PRODUCER_ ]]; then
+    echo "[configuring producer] processing $env_var"
     key=$(get_key $env_var)
     val=${!env_var}
     echo "[configuring producer] '$key'='$val'"
