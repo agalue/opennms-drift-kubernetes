e3a8399ae3a1279d154899f6075aed765797cbb0
Alejandro Galue <agalue@opennms.org>
2019-11-07 10:40:01 -0500

    Fixing istio/knative setup script.

--- serverless/setup-istio-knative.sh
+++ serverless/setup-istio-knative.sh
@@ -23,7 +23,9 @@ function header_text {
 serving_version="v0.10.0"
 eventing_version="v0.10.0"
 istio_version="1.3.3"
+domain="aws.agalue.net"
 kafka_server="kafka.opennms.svc.cluster.local:9092"
+onms_url="https://onmsui.$domain/opennms"
 
 header_text "Starting Knative on minikube..."
 
@@ -31,6 +33,7 @@ header_text "Using Knative Serving Version:  ${serving_version}"
 header_text "Using Knative Eventing Version: ${eventing_version}"
 header_text "Using Istio Version:            ${istio_version}"
 header_text "Using Kafka Server:             ${kafka_server}"
+header_text "Using OpenNMS UI Server         ${onms_url}"
 
 header_text "Labeling default namespace w/ istio-injection=enabled"
 kubectl label namespace default istio-injection=enabled
@@ -55,6 +58,23 @@ kubectl apply -f "https://github.com/knative/eventing-contrib/releases/download/
 header_text "Waiting for Knative Eventing to become ready"
 sleep 5; while echo && kubectl get pods -n knative-eventing | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
 sleep 5; while echo && kubectl get pods -n knative-sources | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
+
+header_text "Configuring custom domain"
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: config-domain
+  namespace: knative-serving
+data:
+  $domain: ""
+EOF
+
+header_text "Creating secret with OpenNMS and Slack URLs"
+kubectl create secret generic serverless-config \
+ --from-literal=SLACK_URL="${slack_url}" \
+ --from-literal=ONMS_URL="${onms_url}" \
+ --dry-run -o yaml | kubectl apply -f -
 
 header_text "Launching Slack Forwarder Service"
 kubectl apply -f knative-service.yaml
